724972ed407253572550e7bdfde5d2c483ff4046
MeltedPenguin <diablo22007@gmail.com>
2016-09-21 23:41:56 +0200

    Call VLC with subtitles in arg; now default (d)elete file after stream

--- faststream.py
+++ faststream.py
@@ -13,35 +13,39 @@ def main(filePath):
     targetPath = tempfile.gettempdir()
     filename = os.path.basename(filePath).split("?")[0]
     videoPath = urllib.parse.unquote(os.path.join(targetPath, filename))
+    languages = ["eng"]
 
     ariaProcess = subprocess.Popen(
         ["aria2c", "-x 8", "--file-allocation=none", "--continue=true", "--stream-piece-selector=inorder", "--dir=" + targetPath, filePath],
         stdout=sys.stdout,
         stderr=sys.stderr)
 
+    time.sleep(2)
+
+    """
     subliminalProcess = subprocess.Popen(
         ["subliminal", "download", "-l", "en", "-s", videoPath],
         stdout=sys.stdout,
         stderr=sys.stderr)
-
     """
+
     region.configure('dogpile.cache.dbm', arguments={'filename': 'cachefile.dbm'})
     video = scan_video(videoPath)
     print(video.video_codec)
     print(video.release_group)
-    subtitles = download_best_subtitles([video], {Language('eng')})
+    subtitles = download_best_subtitles([video], {Language(languages[0])})
     if len(subtitles) > 0:
         print("A subtitle has been found!")
         save_subtitles(video, subtitles[video])
     else:
         print("No subtitles found.")
-    """
 
+    subtitle_path = os.path.splitext(videoPath)[0] + "." + languages[0][:2] + ".srt"
 
     # Let it load a bit
     time.sleep(5)
 
-    vlcProcess = subprocess.Popen(["vlc", videoPath])
+    vlcProcess = subprocess.Popen(["vlc", "--sub-file=" + subtitle_path, videoPath])
 
     vlcProcess.wait()
     print("VLC process finished.")
@@ -49,14 +53,14 @@ def main(filePath):
     ariaProcess.terminate()
 
     while True:
-        res = input("Move the video to the (t)rash, (d)elete or do (n)othing? (default 't') [t/d/n] ")
+        res = input("Move the video to the (t)rash, (d)elete or do (n)othing? (default 'd') [t/d/n] ")
 
-        if (res == "t" or res == ""):
+        if (res == "t"):
             send2trash(videoPath)
             sys.exit(0)
         elif (res == "n"):
             sys.exit(0)
-        elif (res == "d"):
+        elif (res == "d" or res == ""):
             os.remove(videoPath)
             sys.exit(0)
         else:
